ef80ab7f9b186b940cb1ff5c9c29c0d4c31d5f08
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-21 23:37:17 -0300

    Github auth working

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -94,7 +94,9 @@ const Auth = () => {
               <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                 <FcGoogle size={30} />
               </div>
-              <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+              <div
+                onClick={() => signIn("github", { callbackUrl: "/" })}
+                className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
                 <FaGithub size={30} />
               </div>
             </div>
