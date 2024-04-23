5a89480994c7dbd94e7f5a9b34b6f70dcdfa7917
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-04-23 00:52:27 -0300

    Favorites movie list

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 import MovieList from "@/components/MovieList";
 import useMovieList from "@/hooks/useMovieList";
+import useFavorites from "@/hooks/useFavorites";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -24,6 +25,7 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
 
   return (
     <>
@@ -32,6 +34,7 @@ export default function Home() {
 
       <div className="pb-40">
         <MovieList title="Trending Now" data={movies} />
+        <MovieList title="My List" data={favorites} />
       </div>
     </>
   );
